d7588095381acf48ecc90eeef892288b23b88814
Kurtis Van Gent <31518063+kurtisvg@users.noreply.github.com>
2018-06-29 12:00:50 -0700

    Restructured and added ORM for better scaling. (#5)
    * Restructured and added ORM for better scaling.
    
    * Fixes in api.
    
    * Small docker optimizations.
    
    * Add request timer to pages.
    
    * Update README with more complete instructions.

--- app/main.py
+++ app/main.py
@@ -12,182 +12,156 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-import argparse
-
 import os
-import pg8000
-from flask import (
-    Flask, request, g, redirect, url_for,  render_template, jsonify)
-import app.memegenerator as memegenerator
-import app.data_conn as data_conn
+import time
 
-DATABASE = 'memegen.db'
+from flask import (
+    Flask, abort, request, redirect, url_for, render_template, g,
+    send_from_directory)
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy.sql.expression import func
+from PIL import Image, ImageDraw, ImageFont
+
+from configuration import (
+    get_args, get_db_uri, get_templates_list,
+    BASE_DIR, MEME_DIR, FONT_PATH)
 
 app = Flask(__name__)
-app.config.from_object(__name__)
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['SQLALCHEMY_DATABASE_URI'] = get_db_uri()
+db = SQLAlchemy(app)
+
+
+# Model for representing created Memes
+class Meme(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    template = db.Column(db.String(80), nullable=False)
+    top_text = db.Column(db.String(80), nullable=False)
+    bot_text = db.Column(db.String(80), nullable=False)
+
+    def __repr__(self):
+        return '<Meme %r>' % self.id
+
+
+@app.before_first_request
+def setup_db():
+    # Create folder for memes if it doesn't exist
+    if not os.path.exists(MEME_DIR):
+        os.makedirs(MEME_DIR)
+    # Create tables for models if they don't exist
+    db.create_all()
+
+
+@app.before_request
+def setup_request_time():
+    start_time = time.time()
+    g.request_time = lambda: "%d ms" % ((time.time() - start_time) * 1000)
 
 
 @app.route('/')
 def index():
-    get_health()
-    return redirect(url_for("get_images"))
-
-
-def get_cursor():
-    cursor = getattr(g, '_database', None)
-
-    if cursor is None:
-        args = get_args()
-        conn = pg8000.connect(
-            user=args.db_user, password=args.db_pass, host=args.db_host)
-        conn.autocommit = True
-        cursor = g._database = conn.cursor()
-        # Verify the schema is created
-        data_conn.create_schema(cursor)
-
-    return cursor
-
-
-@app.teardown_appcontext
-def close_connection(exception):
-    db = getattr(g, '_database', None)
-    if db is not None:
-        db.close()
-
-
-@app.route('/health', methods=['GET'])
-def get_health():
-    for i in range(3):
-        try:
-            result = data_conn.get_health(get_cursor())
-            if result[0]:
-                return jsonify({
-                    'health': 'OK',
-                    'message': 'Attempt {} of 3.'.format(i)
-                })
-            print("Result failed: {}".format(result))
-        except Exception as e:
-            print("Exception during health check encountered: {}".format(e))
-
-    return jsonify({'health': 'FAIL'})
+    return redirect(url_for("get_create_menu"))
 
 
 @app.route('/recent', methods=['GET'])
-def get_recent():
-
-    # todo(sixela): create table for recent and populate it
-    # everytime a meme is created
-    db_images = data_conn.get_recent(get_cursor())
-    images = list()
-    for image in db_images:
-        html_image = dict()
-        html_image['id_url'] = url_for('get_image', image_id=image[0])
-        html_image['img_url'] = url_for('')
-
-        images.append(html_image)
-
-    return render_template('recent.html')
+def view_recent():
+    memes = Meme.query.order_by(Meme.id.desc()).limit(20).all()
+    return render_template('recent.html', memes=memes)
 
 
-@app.route('/image', methods=['GET'])
-def get_images():
-    db_images = data_conn.get_images(get_cursor())
-    form_data = dict()
-    images = list()
-    for image in db_images:
-        html_image = dict()
-        html_image['id_url'] = url_for('get_image', image_id=image[0])
-        html_image['img_url'] = url_for('static',
-                                        filename='images/%s' % image[1])
-        images.append(html_image)
-    form_data['images'] = images
-    form_data['to_upload'] = True
-    return render_template('grid.html', form_data=form_data)
+@app.route('/random', methods=['GET'])
+def view_random():
+    meme = Meme.query.order_by(func.random()).first()
+    return redirect(url_for('view_meme', meme_id=meme.id))
 
 
-@app.route('/image/<int:image_id>', methods=['GET'])
-def get_image(image_id):
-    image = dict()
-    image['id'] = image_id
-    image['name'] = data_conn.get_image_path(get_cursor(), image_id)
-    return render_template('make_meme.html', image=image)
+@app.route('/template', methods=['GET'])
+def get_create_menu():
+    templates = get_templates_list()
+    return render_template('view_templates.html', templates=templates)
 
 
-@app.route('/image', methods=['POST'])
-def post_images():
-    img = request.files['image']
-    img.save('static/images/%s' % img.filename)
-    img_id = data_conn.create_image(get_cursor(), img.filename)
-    return redirect(url_for("get_image", image_id=img_id))
+@app.route('/template/<string:template>', methods=['GET'])
+def get_create(template):
+    if template not in get_templates_list():
+        abort(400, "Template does not exist.")
+    return render_template('create_meme.html', template=template)
 
 
 @app.route('/meme/<int:meme_id>', methods=['GET'])
-def get_meme(meme_id):
-    return redirect(url_for('static', filename='memes/%d.png' % meme_id))
-
-
-@app.route('/meme', methods=['GET'])
-def get_memes():
-    db_images = data_conn.get_memes(get_cursor())
-    form_data = dict()
-    images = list()
-    for image in db_images:
-        html_image = dict()
-        html_image['img_url'] = url_for('static',
-                                        filename='memes/%d.png' % image[0])
-        html_image['id_url'] = html_image['img_url']
-        images.append(html_image)
-
-    form_data['images'] = images
-    form_data['to_upload'] = False
-    return render_template('grid.html', form_data=form_data)
+def view_meme(meme_id):
+    meme_file = os.path.join(MEME_DIR, '%d.png' % meme_id)
+    if not os.path.exists(meme_file):
+        generate_meme(meme_file, meme_id)
+    print(meme_file)
+    return send_from_directory(MEME_DIR, '%d.png' % meme_id)
 
 
 @app.route('/meme', methods=['POST'])
-def post_meme():
-    meme_id = data_conn.create_meme(
-        get_cursor(), int(request.form['image']),
-        request.form['top'], request.form['bottom'])
-    image_name = data_conn.get_image_path(
-        get_cursor(), int(request.form['image']))
-    memegenerator.gen_meme(
-        image_name, request.form['top'], request.form['bottom'], meme_id)
-    return redirect(url_for('static', filename='memes/%d.png' % meme_id))
+def create_meme():
+    try:
+        meme = Meme(
+            template=request.form['template'],
+            top_text=request.form['top'],
+            bot_text=request.form['bottom']
+        )
+        db.session.add(meme)
+        db.session.commit()
+
+        return redirect(url_for('view_meme', meme_id=meme.id))
+    except KeyError:
+        abort(400, "Incorrect parameters.")
 
 
-def get_args():
-    args = getattr(get_args, '_args', None)
-    if args is None:
-        parser = argparse.ArgumentParser()
+def generate_meme(file, meme_id):
+    # Query for meme
+    meme = Meme.query.filter(Meme.id == meme_id).first()
+    if meme is None:
+        abort(400, 'Meme does not exist.')
+    # Load template
+    template_file = os.path.join(
+        BASE_DIR, 'static', 'templates', meme.template)
+    if not os.path.exists(template_file):
+        abort(400, 'Template does not exist')
+    template = Image.open(template_file)
+    # Get Font Details
+    font, top_loc, bot_loc = calc_font_details(
+        meme.top_text, meme.bot_text, template.size)
+    draw = ImageDraw.Draw(template)
+    draw_text(draw, top_loc[0], top_loc[1], meme.top_text, font)
+    draw_text(draw, bot_loc[0], bot_loc[1], meme.bot_text, font)
 
-        # Only used when running the flask debugging server
-        parser.add_argument(
-            '-H', '--host', default='127.0.0.1',
-            help='Address for the debug webserver to listen on.')
-        parser.add_argument(
-            '-P', '--port', default='8080', type=int,
-            help='Port for webserver to listen on.')
+    template.save(file)
 
-        parser.add_argument(
-            '-d', '--db-host',
-            default=os.environ.get('DB_HOST', '127.0.0.1'),
-            help='Host address of the database.')
-        parser.add_argument(
-            '-u', '--db-user',
-            default=os.environ.get('DB_USER', 'postgres'),
-            help='User used to connect to the database.')
-        parser.add_argument(
-            '-p', '--db-pass',
-            default=os.environ.get('DB_PASS', 'password'),
-            help='Password used to connect to the database.')
 
-        args = get_args._args = parser.parse_args()
-    return args
+# Calculate font size and location
+def calc_font_details(top, bot, img_size):
+    font_size = 50
+    font = ImageFont.truetype(FONT_PATH, font_size)
+    max_width = img_size[0] - 20
+    # Get ideal font size
+    while font.getsize(top)[0] > max_width or font.getsize(bot)[0] > max_width:
+        font_size = font_size - 1
+        font = ImageFont.truetype(FONT_PATH, font_size)
+    # Get font locations
+    top_loc = ((img_size[0] - font.getsize(top)[0])/2, -5)
+    bot_size = font.getsize(bot)
+    bot_loc = ((img_size[0] - bot_size[0])/2, img_size[1] - bot_size[1] - 5)
+    return font, top_loc, bot_loc
+
+
+# Draws the given text with a border
+def draw_text(draw, x, y, text, font):
+    # Draw border
+    draw.text((x-1, y-1), text, font=font, fill="black")
+    draw.text((x+1, y-1), text, font=font, fill="black")
+    draw.text((x-1, y+1), text, font=font, fill="black")
+    draw.text((x+1, y+1), text, font=font, fill="black")
+    # Draw text
+    draw.text((x, y), text, font=font, fill="white")
 
 
 if __name__ == '__main__':
-
+    # Run dev server (for debugging only)
     args = get_args()
-
-    app.debug = True
-    app.run(host=args.host, port=args.port)
+    app.run(host=args.host, port=args.port, debug=True)
